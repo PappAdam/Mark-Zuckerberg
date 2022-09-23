425f35b2339312067a0b3371f289bb38520101fc
Papp Ádám <Papp.Adam@students.jedlik.eu>
2022-09-23 11:14:27 +0200

    basic layout

--- typing.js
+++ typing.js
@@ -1,11 +1,15 @@
 var i = 0;
+var text = "Some text";
 
-function Write() {
-    var text = "Some text";
-    if (i < text.length) {
-        var speed = Math.floor(Math.random() * 80) + 30;
-        setTimeout(Write, speed);
-        document.getElementById("typing").innerHTML += text.charAt(i);
-        i++;
-    }
+function write(id, _text) {
+    console.log(_text);
+    document.getElementById(id).innerText += _text.substring(0, 1);
+    var speed = Math.floor(Math.random() * 80) + 30;
+    if (_text.length > 1)
+        setTimeout(write, speed, id, _text.substring(1));
+}
+
+
+function h1Write() {
+    document.querySelectorAll('h1').forEach(h1 => write(h1.id, text));
 }
