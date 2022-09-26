fc404c13c51791528c1db67a3ced0726f35ef42e
Csizmadia Barnabas <cb20040621@gmail.com>
2022-09-26 09:40:06 +0200

    started work on issue #14 and #15

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,21 @@ import Placehodler from './transitions/default.js';
 import Highway from '@dogstudio/highway';
 import { SwipeRight, SwipeLeft } from './transitions/swipe.js';
 
+let load = 0;
+
+const pageAccessedByReload = (
+    (window.performance.navigation && window.performance.navigation.type === 1) ||
+    window.performance
+        .getEntriesByType('navigation')
+        .map((nav) => nav.type)
+        .includes('reload')
+);
+
+if (pageAccessedByReload === true) {
+    window.location.replace('http://localhost:1234/index.html');
+}
+
+
 const H = new Highway.Core({
     transitions: {
         default: Placehodler,
--- javascript/navbar/position.js
+++ javascript/navbar/position.js
@@ -1,27 +1,28 @@
 export function swipe_direction() {
 
+    let location = window.location.href.split("/").pop();
     let status = document.getElementById("status_nav");
     let learn = document.getElementById("learn_nav");
     let news = document.getElementById("news_nav");
 
-    if (window.location.href.split("/").pop() === "index.html") {
-        console.log(window.location.href.split("/").pop());
+    if (location === "index.html") {
+        console.log(location);
         status.setAttribute("data-transition", "swipe_r");
         learn.setAttribute("data-transition", "swipe_r");
         news.setAttribute("data-transition", "swipe_r");
 
-    } else if (window.location.href.split("/").pop() === "status.html") {
-        console.log(window.location.href.split("/").pop());
+    } else if (location === "status.html") {
+        console.log(location);
         learn.setAttribute("data-transition", "swipe_r");
         news.setAttribute("data-transition", "swipe_r");
 
-    } else if (window.location.href.split("/").pop() === "learn.html") {
-        console.log(window.location.href.split("/").pop());
+    } else if (location === "learn.html") {
+        console.log(location);
         status.setAttribute("data-transition", "swipe_l");
         news.setAttribute("data-transition", "swipe_r");
 
     } else {
-        console.log(window.location.href.split("/").pop());
+        console.log(location);
         learn.setAttribute("data-transition", "swipe_l");
         status.setAttribute("data-transition", "swipe_l");
     }
