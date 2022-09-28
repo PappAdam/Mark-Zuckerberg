2daee8c8d0e247922919cd7baa1941c0470e5075
Papp Adam <papp.adam@students.jedlik.eu>
2022-09-28 19:58:10 +0200

    content swap on focus + header in editor

--- dist/index.d76c80b4.js
+++ dist/index.d76c80b4.js
@@ -3,6 +3,12 @@ function button_press(button) {
         if (document.getElementById("focus")) document.getElementById("focus").removeAttribute("id");
         button.parentElement.parentElement.parentElement.setAttribute("id", "focus");
     } else if (button.parentElement.parentElement.parentElement.id == "focus" && document.getElementById("focus")) document.getElementById("focus").removeAttribute("id");
+    setTimeout(fill_content, 500);
+}
+function fill_content() {
+    var elements = document.querySelectorAll(".content");
+    for(var i = 0; i < elements.length; i++)if (elements[i].parentElement.id == "focus") elements[i].innerHTML = "<p>text</p><p>when</p><p>this window is</p><p>focused</p>";
+    else elements[i].innerHTML = "<p>text</p><p>when</p><p>it isn't</p>";
 }
 
 //# sourceMappingURL=index.d76c80b4.js.map
--- javascript/cards/editor.js
+++ javascript/cards/editor.js
@@ -4,10 +4,24 @@ function button_press(button) {
             document.getElementById("focus").removeAttribute("id");
         }
         button.parentElement.parentElement.parentElement.setAttribute('id', 'focus');
+
     }
     else {
         if (button.parentElement.parentElement.parentElement.id == "focus" && document.getElementById("focus")) {
             document.getElementById("focus").removeAttribute("id");
+        }
+    }
+    setTimeout(fill_content, 500);
+}
+
+function fill_content() {
+    var elements = document.querySelectorAll('.content');
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].parentElement.id == 'focus') {
+            elements[i].innerHTML = '<p>text</p><p>when</p><p>this window is</p><p>focused</p>'
+        }
+        else {
+            elements[i].innerHTML = '<p>text</p><p>when</p><p>it isn\'t</p>'
         }
     }
 }
