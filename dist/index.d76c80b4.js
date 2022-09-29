2b7915a1ce3e9569a56d43f7d8e919819f131c3f
Papp Adam <papp.adam@students.jedlik.eu>
2022-09-29 20:38:37 +0200

    Window content can be customized

--- dist/index.d76c80b4.js
+++ dist/index.d76c80b4.js
@@ -1,14 +1,27 @@
+var contents = [
+    "id=1;Text when window is not on focus;Text when the :nwindow is on focus",
+    "id=2;Szia:nBarnika;Pancser:n:nvagy",
+    "id=3;Hahah :nHehe;XDDDDDD :nHUHU", 
+];
 function button_press(button) {
     if (button.classList.contains("fullscreen")) {
         if (document.getElementById("focus")) document.getElementById("focus").removeAttribute("id");
         button.parentElement.parentElement.parentElement.setAttribute("id", "focus");
     } else if (button.parentElement.parentElement.parentElement.id == "focus" && document.getElementById("focus")) document.getElementById("focus").removeAttribute("id");
-    setTimeout(fill_content, 500);
+    setTimeout(fill_content, 250);
 }
 function fill_content() {
     var elements = document.querySelectorAll(".content");
-    for(var i = 0; i < elements.length; i++)if (elements[i].parentElement.id == "focus") elements[i].innerHTML = "<p>text</p><p>when</p><p>this window is</p><p>focused</p>";
-    else elements[i].innerHTML = "<p>text</p><p>when</p><p>it isn't</p>";
+    for(var i = 0; i < elements.length; i++)if (elements[i].parentElement.id == "focus") elements[i].innerHTML = parse_content(elements[i].id, 0);
+    else elements[i].innerHTML = parse_content(elements[i].id, 1);
+}
+function parse_content(id, focus) {
+    var parsed_content = "";
+    for(var i = 0; i < contents.length; i++)if (contents[i].includes("id=" + id)) {
+        var content = contents[i].split(";")[focus + 1].split(":n");
+        for(var j = 0; j < content.length; j++)parsed_content += "<p>" + content[j] + "</p>";
+    }
+    return parsed_content;
 }
 
 //# sourceMappingURL=index.d76c80b4.js.map
--- javascript/cards/editor.js
+++ javascript/cards/editor.js
@@ -1,3 +1,9 @@
+var contents = [
+    "id=1;Text when window is not on focus;Text when the :nwindow is on focus",
+    "id=2;Szia:nBarnika;Pancser:n:nvagy",
+    "id=3;Hahah :nHehe;XDDDDDD :nHUHU",
+];
+
 function button_press(button) {
     if (button.classList.contains('fullscreen')) {
         if (document.getElementById("focus")) {
@@ -11,17 +17,33 @@ function button_press(button) {
             document.getElementById("focus").removeAttribute("id");
         }
     }
-    setTimeout(fill_content, 500);
+    setTimeout(fill_content, 250);
 }
 
 function fill_content() {
     var elements = document.querySelectorAll('.content');
     for (var i = 0; i < elements.length; i++) {
         if (elements[i].parentElement.id == 'focus') {
-            elements[i].innerHTML = '<p>text</p><p>when</p><p>this window is</p><p>focused</p>'
+            elements[i].innerHTML = parse_content(elements[i].id, 0);
         }
         else {
-            elements[i].innerHTML = '<p>text</p><p>when</p><p>it isn\'t</p>'
+            elements[i].innerHTML = parse_content(elements[i].id, 1);
         }
     }
+}
+
+
+function parse_content(id, focus) {
+    var parsed_content = "";
+    for (var i = 0; i < contents.length; i++) {
+        if (contents[i].includes("id="+id)) {
+            var content = contents[i].split(';')[focus+1].split(':n');
+
+            for (var j = 0; j < content.length; j++) {
+                parsed_content += "<p>" + content[j] + "</p>";
+            }
+        }
+    }
+    
+    return parsed_content;
 }
