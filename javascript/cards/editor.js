d770620e7126c82c8bfb8369d523f2caf05fef2e
Papp Ádám <Papp.Adam@students.jedlik.eu>
2022-09-30 09:48:05 +0200

    minor changes + stylized editor content

--- javascript/cards/editor.js
+++ javascript/cards/editor.js
@@ -1,8 +1,10 @@
 var contents = [ // :n => new line; first part => unfocused sec part => focused (separeted by ;) 
     "id=1;Text when window is not on focus;Text when the :nwindow is on focus",
-    "id=2;Szia:nBarnika;Pancser:n:nvagy",
-    "id=3;Hahah :nHehe;XDDDDDD :nHUHU",
+    "id=2;Szia:nBarni;|add:=style='color: #678'|Some| very long text in the content div of the terminal window", //to add style use |add:={*style*}| before the text beginning and | at the and of the text
+    "id=3;Hahah :nHehe;|add:=style='color: red'|Lorem ipsum dolor sit amet consectetur adipisicing elit.| Eveniet numquam nisi illum aut ipsa veritatis voluptas nulla facere at autem explicabo officiis nam dolorum saepe eaque, et quod minus sint!",
 ];
+
+var XPressed = 0;
 
 function button_press(button) {
     if (button.classList.contains('fullscreen')) {
@@ -13,10 +15,16 @@ function button_press(button) {
 
     }
     else {
+        XPressed++;
         if (button.parentElement.parentElement.parentElement.id == "focus" && document.getElementById("focus")) {
             document.getElementById("focus").removeAttribute("id");
         }
+        else if (XPressed%10 == 0 && XPressed != 0) {
+            alert("MEGIS MI A FASZT VARSZ HA EDDIG NEM MUKODOTT?");
+        }
     }
+
+
     setTimeout(fill_content, 250);
 }
 
@@ -32,7 +40,6 @@ function fill_content() {
     }
 }
 
-
 function parse_content(id, focus) {
     var parsed_content = "";
     for (var i = 0; i < contents.length; i++) {
@@ -40,10 +47,25 @@ function parse_content(id, focus) {
             var content = contents[i].split(';')[focus+1].split(':n');
 
             for (var j = 0; j < content.length; j++) {
-                parsed_content += "<p>" + content[j] + "</p>";
+                var styled = content[j].split("|");
+                var style_close = false;
+                var s_content = "<p>";
+                for (var k = 0; k < styled.length; k++) {
+                    if (styled[k].includes('add:=')) {
+                        s_content += "<span " + styled[k].substring(5) + ">";
+                        style_close = true;
+                    }
+                    else if (style_close) {
+                        s_content += styled[k] + "</span>";
+                        style_close = false;
+                    }
+                    else {
+                        s_content += styled[k];
+                    }
+                }
+                parsed_content += s_content + "</p>";
             }
         }
     }
-
     return parsed_content;
 }
